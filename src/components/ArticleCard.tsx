d584c59c2957cf275bbfc5f74294ddeadf178c89
omarkhaled11 <omarkhaled320@hotmail.com>
2020-05-05 20:47:05 +0200

    Add article card initial UI

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+import { theme } from '../theme';
 
 const ArticleCard = ({ article }) => {
   const { title, slug } = article;
   return (
-    <li>
-      <h1>{title}</h1>
-      <Link href="/blog/[slug]" as={`/blog/${slug}`}>
-        <a>{slug}</a>
-      </Link>
-    </li>
+    <Link href="/blog/[slug]" as={`/blog/${slug}`}>
+      <CardContainer>
+        <ImageBackground />
+        <CardTitle>{title} asdasdasd asdasdasd</CardTitle>
+      </CardContainer>
+    </Link>
   );
 }
+
+const CardContainer = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding-bottom: ${theme.metrics.m2};
+  border: 1px solid ${theme.colors.border};
+  width: ${theme.dimensions.imageWidth};
+  margin-right: ${theme.metrics.m2};
+  margin-bottom: ${theme.metrics.m2};
+  word-wrap: break-word;
+  :hover {
+    cursor: pointer;
+  };
+`
+
+const CardTitle = styled.p`
+  padding: ${theme.metrics.m1};
+  font-size: ${theme.fonts.large};
+`
+
+const ImageBackground = styled.div`
+  height: ${theme.dimensions.imageHeight};
+  width: ${theme.dimensions.imageWidth};
+  background-image: url("https://image.shutterstock.com/image-photo/forest-road-under-sunset-sunbeams-260nw-228722404.jpg");
+`
 
 export default ArticleCard;
