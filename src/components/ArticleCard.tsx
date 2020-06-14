0532fe903712c5382bbc43246cdc7137d105ee3c
abdelrahmanahmed4200 <ahmed.abdelrahman@mytoys.de>
2020-06-14 20:26:01 +0200

    added articles section styling

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,77 +1,27 @@
-import React from 'react';
 import Link from 'next/link';
-import styled from 'styled-components';
-import { theme } from '../theme';
+
+import styles from './ArticleCard.module.scss';
 
 const ArticleCard = ({ article }) => {
   const { title, slug, discipline, readingTimeInMinutes } = article;
+
   return (
     <Link href="/blog/[slug]" as={`/blog/${slug}`}>
-      <CardContainer>
-        <Discipline>{discipline.disciplineName}</Discipline>
-        <Title>{title}</Title>
-        <InfoRow>
-            <InfoLabel>مدة القراءة:</InfoLabel>
-            <InfoValue> {readingTimeInMinutes} دقائق </InfoValue>
-          </InfoRow>
-      </CardContainer>
+      <div className={styles.container}>
+        <div className={styles.discipline}>{discipline.disciplineName}</div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.reading_time}>
+          <div>
+            مدة القراءة:{' '}
+            <span className={styles.reading_time_value}>
+              {readingTimeInMinutes} دقائق
+            </span>
+          </div>
+        </div>
+        <div className={styles.go_to}>إلى المقال</div>
+      </div>
     </Link>
   );
-}
-
-const CardContainer = styled.a`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  padding: ${theme.metrics.smallMargin};
-  border: 1px solid ${theme.colors.secondary};
-  width: ${theme.dimensions.imageWidth};
-  margin-bottom: ${theme.metrics.m3};
-  word-wrap: break-word;
-  :hover {
-    cursor: pointer;
-    border-color: ${theme.colors.themeRed};
-    span {
-      color: ${theme.colors.themeRed};
-    }
-  };
-`
-
-const Title = styled.span`
-  font-size: ${theme.fonts.medium};
-  color: ${theme.colors.primary};
-`
-
-const Discipline = styled.span`
-  padding-bottom: ${theme.metrics.m1};
-  font-size: ${theme.fonts.small};
-  font-family: ${theme.fonts.bold};
-  color: ${theme.colors.primary};
-`
-
-const InfoRow = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  margin-top: ${theme.metrics.m2};
-`;
-
-const InfoLabel = styled.span`
-  font-size: ${theme.fonts.small};
-  color: ${theme.colors.primary};
-`;
-
-const InfoValue = styled.span`
-  font-family: ${theme.fonts.bold};
-  font-size: ${theme.fonts.small};
-  color: ${theme.colors.primary};
-  padding-right: ${theme.metrics.m1};
-`;
-
-// const ImageBackground = styled.div`
-//   height: ${theme.dimensions.imageHeight};
-//   width: ${theme.dimensions.imageWidth};
-//   background-image: url("https://image.shutterstock.com/image-photo/forest-road-under-sunset-sunbeams-260nw-228722404.jpg");
-// `
+};
 
 export default ArticleCard;
