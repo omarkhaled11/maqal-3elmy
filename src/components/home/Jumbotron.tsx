588e528873161d61ecc3810258c1e56017ae3347
abdelrahmanahmed4200 <ahmed.abdelrahman@mytoys.de>
2020-06-14 16:55:31 +0200

    added read more section

--- src/components/home/Jumbotron.tsx
+++ src/components/home/Jumbotron.tsx
@@ -9,12 +9,11 @@ const ArticleCard = () => {
         <p className={styles.text}>
           نحن ١٠٠ باحث و باحثة نكتب عن المقالات العلمية بشكل مبسط
         </p>
+
         {/* <img src="/images/home.png"></img> */}
       </div>
-      <div>
-        {/* <ReadMoreText>
-
-            </ReadMoreText> */}
+      <div className={styles.read_more_container}>
+        <p className={styles.read_more}>إقرأ المزيد</p>
         {/* Icon */}
       </div>
     </div>
