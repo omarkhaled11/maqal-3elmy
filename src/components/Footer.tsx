66517634168dc490514eb440bc66b67d8cd0565a
omarkhaled11 <omarkhaled320@hotmail.com>
2020-07-18 22:13:48 +0200

    Update footer ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,93 +30,81 @@ const Footer = ({ disciplines }) => {
     []
   );
 
+  const half = Math.ceil(naturalSciencesDisciplines.length / 2);
+
+  const disciplinesArrays1 = naturalSciencesDisciplines.splice(0, half);
+  const disciplinesArrays2 = naturalSciencesDisciplines.splice(-half);
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <div className={styles.logo_container}>
-          <img className={styles.logo} src="/logo/white.svg" alt="logo" />
+        <div className={styles.footer_head}>
+          <p className={styles.footer_head_text}>
+          مقال علمي هي مبادرة علمية رائدة تحوي على أكتر من ١٠٠ باحث و باحثة يعملون في كبرى الجامعات و المعاهد البحثية يبسطون مقالات علمية نُشرت في دوريات علمية مرموقة.
+          </p>
         </div>
-        <div className={styles.links}>
-          <div className={styles.menu}>
-            <nav className={styles.nav}>
-              <Link href="/about">
-                <a>من نحن</a>
-              </Link>
-              <Link href="/disciplineEditors">
-                <a>عن محررينا</a>
-              </Link>
-              <Link href="/">
-                <a>اتصل بنا</a>
-              </Link>
-              <Link href="/">
-                <a>سندات</a>
-              </Link>
-            </nav>
-            <div className={styles.sub_nav}>
-              <ul className={styles.list}>
-                <span className={styles.cluster_title}>العلوم الطبيعية</span>
-                <div className={styles.cluster_items}>
-                  {naturalSciencesDisciplines.map((discipline) => {
-                    return (
-                      <li className={styles.item}>
-                        <a
-                          className={styles.link}
-                          href={`discipline/${discipline.slug}`}
-                        >
-                          {discipline.disciplineName}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </div>
-              </ul>
-              <ul className={styles.list}>
-                <span className={styles.cluster_title}>العلوم الإجتماعية</span>
-                <div className={styles.cluster_items}>
-                  {socialSciencesDisciplines.map((discipline) => {
-                    return (
-                      <li className={styles.item}>
-                        <a
-                          className={styles.link}
-                          href={`discipline/${discipline.slug}`}
-                        >
-                          {discipline.disciplineName}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </div>
-              </ul>
-            </div>
+        <div className={styles.links_container}>
+          <nav className={styles.links_column}>
+            <Link href='/about'>
+              <a className={styles.website_link}>من نحن</a>
+            </Link>
+            <Link href='/editors'>
+              <a className={styles.website_link}>عن محررينا</a>
+            </Link>
+          </nav>
+          <div className={styles.links_column}>
+            {disciplinesArrays1.map(discipline => {
+              return (
+                <Link href={`discipline/${discipline.slug}`}>
+                  <a>{discipline.disciplineName}</a>
+                </Link>
+              );
+            })}
           </div>
+          <div className={styles.links_column}>
+            {disciplinesArrays2.map(discipline => {
+              return (
+                <Link href={`discipline/${discipline.slug}`}>
+                  <a>{discipline.disciplineName}</a>
+                </Link>
+              );
+            })}
+            {socialSciencesDisciplines.map(discipline => {
+              return (
+                <Link href={`discipline/${discipline.slug}`}>
+                  <a>{discipline.disciplineName}</a>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <hr className={styles.divider}></hr>
         </div>
         <div className={styles.social_media_container}>
           <a href={FACEBOOK_LINK}>
             <img
               className={styles.social_media}
-              src="/icons/fb.svg"
-              alt="facebook"
+              src='/icons/fb.svg'
+              alt='facebook'
             />
           </a>
           <a href={LINKEDIN_LINK}>
             <img
               className={styles.social_media}
-              src="/icons/li.svg"
-              alt="linkedin"
+              src='/icons/li.svg'
+              alt='linkedin'
             />
           </a>
           <a href={TWITTER_LINK}>
             <img
               className={styles.social_media}
-              src="/icons/tw.svg"
-              alt="twitter"
+              src='/icons/tw.svg'
+              alt='twitter'
             />
           </a>
         </div>
-        <div>
-          <hr className={styles.divider}></hr>
-        </div>
-        <div className={styles.copyrights}>جميع الحقوق محفوظة</div>
+        <div className={styles.copyrights}>صمم شعار مقال علمي مروى شيخون </div>
       </div>
     </footer>
   );
